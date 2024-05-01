e5463b88f165d42a178f676b242e3baf12a54338
Paulo Henrique <paulohguerra@outlook.com.br>
2024-05-01 00:39:10 -0300

    Configuração da nossa clase User

---
+++ src/main/java/tech/buildrun/agreagadorinvestimentos/entity/User.java
@@ -0,0 +1,91 @@
+package tech.buildrun.agreagadorinvestimentos.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    @UpdateTimestamp
+    private Instant updateTimeStamp;
+
+    public User() {
+    }
+
+    public User(UUID userId, String username, String password, String email, Instant creationTimeStamp, Instant updateTimeStamp) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.creationTimeStamp = creationTimeStamp;
+        this.updateTimeStamp = updateTimeStamp;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Instant getUpdateTimeStamp() {
+        return updateTimeStamp;
+    }
+
+    public void setUpdateTimeStamp(Instant updateTimeStamp) {
+        this.updateTimeStamp = updateTimeStamp;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
