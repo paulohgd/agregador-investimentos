28949d00791ffd45684af23135376fbeb74b7997
Paulo Henrique <paulohguerra@outlook.com.br>
2024-05-06 20:12:48 -0300

    Controller

--- src/main/java/tech/buildrun/agreagadorinvestimentos/controller/UserController.java
+++ src/main/java/tech/buildrun/agreagadorinvestimentos/controller/UserController.java
@@ -1,4 +1,68 @@
 package tech.buildrun.agreagadorinvestimentos.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.buildrun.agreagadorinvestimentos.entity.User;
+import tech.buildrun.agreagadorinvestimentos.repository.UserRepository;
+import tech.buildrun.agreagadorinvestimentos.service.UserService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/users")
 public class UserController {
+
+    private UserService userService;
+
+
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+        userService.CreateUser(createUserDto);
+        var userId = userService.CreateUser(createUserDto);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
+        var user = userService.getUserById(userId);
+        if (user.isPresent()){
+            return ResponseEntity.ok(user.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        var users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+
+    @PutMapping("/update/{userId}")
+    public ResponseEntity<Void> updateUserById(@PathVariable("userId") String userId,
+                                               @RequestBody UpdateUserDto updateUserDto) {
+        userService.updateUserById(userId, updateUserDto);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable("userId") String userId) {
+        var user = userService.getUserById(userId);
+        if (user.isPresent()){
+            userService.deleteUserById(userId);
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
